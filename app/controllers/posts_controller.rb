98653846df112d38ffe64232775aba3aae28bfc3
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-01-04 13:06:41 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,22 @@
-# このファイルはコントローラーである　ファイル名のアンダーバーより左のやつ　postsのこと
-
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-end
 
-# ここで言うモデル名とは、モデルファイルの中のクラス名の事を指す
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+end
